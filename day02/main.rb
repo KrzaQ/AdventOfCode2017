99d768ec45e7d0f9d9b7ec9f9f306b24bf71b1de
KrzaQ <krzaq@krzaq.cc>
2017-12-02 06:43:29 +0100

    Formatting fixes.

--- day02/main.rb
+++ day02/main.rb
@@ -4,7 +4,7 @@ DATA = File.read('data.txt')
 
 
 P1 = DATA
-    .map{|arr| arr.minmax.reverse.inject(:-) }
+    .map{ |arr| arr.minmax.reverse.inject(:-) }
     .sum
 
 def get_div(arr)
@@ -12,12 +12,12 @@ def get_div(arr)
         .combination(2)
         .map(&:sort)
         .map(&:reverse)
-        .find{|k, v| k % v == 0 }
+        .find{ |k, v| k % v == 0 }
     result.first / result.last
 end
 
 P2 = DATA
-    .map{ |a| get_div a}
+    .map{ |a| get_div a }
     .sum
 
 puts "Part 1: %s" % P1
