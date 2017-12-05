7b4ee5e287c15a183a8f7a3ff0c330b6172fbb7e
KrzaQ <krzaq@krzaq.cc>
2017-12-05 06:25:34 +0100

    Day 5 was too wet. DRY now.

--- day05/main.rb
+++ day05/main.rb
@@ -1,32 +1,19 @@
-P1DATA = File.read('data.txt').split.map(&:to_i)
-P2DATA = P1DATA.clone
+DATA = File.read('data.txt').split.map(&:to_i)
 
-i = 0
-count = 0
-while i < P1DATA.size and i >= 0
-    offset = P1DATA[i]
-    P1DATA[i] += 1
-    i = i + offset
-    count += 1
-end
-
-P1 = count
-
-
-i = 0
-count = 0
-while i < P2DATA.size and i >= 0
-    offset = P2DATA[i]
-    if offset < 3
-        P2DATA[i] += 1
-    else
-        P2DATA[i] -= 1
+def solve(data, &blk)
+    i = 0
+    count = 0
+    while i < data.size and i >= 0
+        offset = data[i]
+        data[i] = blk.call data[i]
+        i = i + offset
+        count += 1
     end
-    i = i + offset
-    count += 1
+    count
 end
 
-P2 = count
+P1 = solve(DATA.clone){ |n| n += 1 }
+P2 = solve(DATA.clone){ |n| n += n > 2 ? -1 : 1 }
 
 puts "Part 1: %s" % P1
 puts "Part 2: %s" % P2
