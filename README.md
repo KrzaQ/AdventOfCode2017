248735462a2ca4a6fbfd22b9af1dcb888b95aebf
KrzaQ <krzaq@krzaq.cc>
2017-12-07 06:49:02 +0100

    Day 7 done. Ugly, but works.

--- README.md
+++ README.md
@@ -11,6 +11,7 @@ I'm using various languages to provide solutions.
 | 04  | [High-Entropy Passphrases][day04]              |      |         | [:heavy_check_mark:](./day04/main.cpp) | [:heavy_check_mark:](./day04/main.rb)      |             |
 | 05  | [Maze of Twisty Trampolines, All Alike][day05] |      |         |        | [:heavy_check_mark:](./day05/main.rb)      |             |
 | 06  | [Memory Reallocation][day06]                   |      |         |        | [:heavy_check_mark:](./day06/main.rb)      |             |
+| 07  | [Recursive Circus][day07]                      |      |         |        | [:heavy_check_mark:](./day07/main.rb)      |             |
 
 [day01]: https://adventofcode.com/2017/day/1
 [day02]: https://adventofcode.com/2017/day/2
@@ -18,6 +19,7 @@ I'm using various languages to provide solutions.
 [day04]: https://adventofcode.com/2017/day/4
 [day05]: https://adventofcode.com/2017/day/5
 [day06]: https://adventofcode.com/2017/day/6
+[day07]: https://adventofcode.com/2017/day/7
 
 [kq]: https://dev.krzaq.cc
 
---
+++ day07/main.rb
@@ -0,0 +1,70 @@
+DATA = File.read('data.txt').each_line.map(&:strip).to_a
+
+
+# class Graph
+
+
+    # def scan_line line
+        # /(\w+) \((\d+)\)/
+    # end
+# end
+# 
+# g = Graph.new
+
+graph = {}
+rev = {}
+
+DATA
+    .join("\n")
+    .scan(/^(\w+) \(\d+\) -> (.+)$/)
+    .each do |k, v|
+        graph[k] = v.split(", ")
+        graph[k].each do |r|
+            rev[r] = k
+        end
+    end
+    # .map{ |k, v| { node: k, children: v.split(", ") } }
+# p graph
+
+
+
+
+
+f = graph.keys.first
+top = f
+while rev.fetch(f, nil) do 
+    f = rev[f]
+    top = f
+end
+
+p top
+
+
+weights = DATA
+    .join("\n")
+    .scan(/^(\w+) \((\d+)\)/)
+    .map{ |k, v| [k, v.to_i] }
+    .to_h
+
+
+$cache = {}
+def calc_children_weight(w, g, name)
+    return $cache[name] if $cache.has_key? name
+    result = g.fetch(name, []).map{ |n| calc_children_weight(w, g, n) }.sum + w[name]
+    $cache[name] = result
+    result
+end
+
+incorrect = graph.keys
+    .select{ |k| x = graph[k].map{ |c| calc_children_weight(weights, graph, c) }.sort; x.first != x.last }
+
+p incorrect
+p incorrect.reject{ |n| graph[n].any?{ |c| incorrect.include? c } }
+
+p calc_children_weight(weights, graph, 'aurik')
+graph['aurik'].each do |c| p [c, calc_children_weight(weights, graph, c)] end
+
+# p calc_children_weight(weights, graph, top)
+
+# puts "Part 1: %s" % P1
+# puts "Part 2: %s" % P2
