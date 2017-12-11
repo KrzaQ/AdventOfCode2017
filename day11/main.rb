5e8d9d6778d1a7b3dc31c234604ff3d26f69093a
KrzaQ <krzaq@krzaq.cc>
2017-12-11 14:36:30 +0100

    Made day 11 kosher.

--- day11/main.rb
+++ day11/main.rb
@@ -1,75 +1,22 @@
-require 'set'
-
-# DATA = 'ne,ne,ne'
 DATA = File.read('data.txt')
     .strip
     .split(",")
     .map(&:to_sym)
 
-def coord_offset(direction, from)
-    case direction
-    when :se
-        from.first % 2 == 0 ?  [1, 0] : [1, 1]
-    when :s
-        [0, 1]
-    when :sw
-        from.first % 2 == 0 ?  [-1, 0] : [-1, 1]
-    when :nw
-        from.first % 2 == 0 ?  [-1, -1] : [-1, 0]
-    when :n
-        [0, -1]
-    when :ne
-        from.first % 2 == 0 ?  [1, -1] : [1, 0]
-    end
-end
+DIRECTIONS = {
+    n:  [ 0,  1, -1],
+    ne: [ 1,  0, -1],
+    se: [ 1, -1,  0],
+    s:  [ 0, -1,  1],
+    sw: [-1,  0,  1],
+    nw: [-1,  1,  0],
+}
 
-def move_coord(direction, from)
-    coord_offset(direction, from).zip(from).map(&:sum)
-end
+DISTANCES = DATA
+    .inject([[0,0,0]]){ |l, dir| l.push DIRECTIONS[dir].zip(l.last).map(&:sum) }
+    .map{ |arr| arr.map(&:abs).sum / 2 }
 
-FINAL_LOCATION = DATA.inject({current: [0, 0], past: []}){ |pos, dir| {current: move_coord(dir, pos[:current]), past: pos[:past] + [pos[:current]] } }
-
-def find_distances
-    distances = { [0, 0] => 0 }
-    directions = [:se, :s, :sw, :nw, :n, :ne]
-    todo = [
-        [0, 0]
-    ]
-
-    final = 0
-    max_dist = 0
-
-    visited = FINAL_LOCATION[:past].to_set
-    visited.add FINAL_LOCATION[:current]
-    visited.delete [0, 0]
-
-    loop do
-        added = []
-        todo.each do |coord|
-            directions.each do |dir|
-                new_coord = move_coord(dir, coord)
-                
-                next if distances.has_key? new_coord
-
-                distances[new_coord] = distances[coord] + 1
-
-                added.push new_coord
-
-                if visited.include? new_coord
-                    max_dist = [max_dist, distances[new_coord]].max
-                    visited.delete new_coord
-                end
-
-                if visited.size == 0
-                    return [distances[FINAL_LOCATION[:current]], max_dist]
-                end
-            end
-        end
-        todo = added.clone
-    end
-end
-
-P1, P2 = find_distances
+P1, P2 = DISTANCES.last, DISTANCES.max
 
 puts "Part 1: %s" % P1
 puts "Part 2: %s" % P2
