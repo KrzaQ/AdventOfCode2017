0a711fae2812f312ea2764d1cf4ef32207591808
KrzaQ <krzaq@krzaq.cc>
2017-12-13 07:27:42 +0100

    Allowed stopping the sim if caught.

--- day13/main.rb
+++ day13/main.rb
@@ -4,13 +4,14 @@ DATA = File.read('data.txt')
     .map{ |line| line.split(': ').map(&:to_i) }
     .to_h
 
-def count_catch_severity(delay)
+def count_catch_severity(delay, bail_early = false)
     DATA.inject([0, false]) do |state, el|
         index, depth = el
         count, caught = state
         if (index+delay) % (2*(depth-1)) == 0
             count += index * depth
             caught = true
+            return [ -1, true ] if bail_early
         end
         [ count, caught ]
     end
@@ -18,7 +19,7 @@ end
 
 P1 = count_catch_severity(0).first
 P2 = (0..Float::INFINITY)
-    .find{ |n| count_catch_severity(n).last == false }
+    .find{ |n| count_catch_severity(n, true).last == false }
 
 puts "Part 1: %s" % P1
 puts "Part 2: %s" % P2
